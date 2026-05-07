c5734b521eab575647fd766c5725073cef731444
anitadhabewar05 <anitadhabewar.05@gmail.com>
2026-05-07 16:35:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3467-transform-array-by-parity/3467-transform-array-by-parity.java
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] transformArray(int[] nums) {
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] % 2 == 0)
-            {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i] % 2 == 0){
                 nums[i] = 0;
-            }
-            else
-            {
+            }else{
                 nums[i] = 1;
             }
         }
-        Arrays.sort(nums);
-
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[j]<nums[i]){
+                    int temp=nums[j];
+                    nums[j]=nums[i];
+                    nums[i]=temp;
+                }
+            }
+        }
         return nums;
     }
 }

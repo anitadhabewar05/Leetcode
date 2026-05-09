af4bdcda0292784d7a21321a17c0eb06c398a0f1
anitadhabewar05 <anitadhabewar.05@gmail.com>
2026-05-09 16:00:58 +0530

    Time: 2 ms (100.00%) | Memory: 46.4 MB (72.26%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -1,21 +1,26 @@
 class Solution {
     public int[] minOperations(String boxes) {
         int n = boxes.length();
-        int ans[] = new int[n];
-
-        for(int i = 0; i < boxes.length(); i++)
-        {
-            int sum = 0;
-            for(int j = 0; j < boxes.length(); j++)
-            {
-                if(boxes.charAt(j) == '1')
-                {
-                    sum = sum + Math.abs(i-j);
-                }
-                ans[i] = sum;
-            }
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)arr[i]=boxes.charAt(i)-'0';
+        int[] pref =new int[n];
+        int[] suf = new int[n];
+        int oneCnt=0;
+        oneCnt = arr[0];
+        // pref[0]=oneCnt;
+        for(int i=1;i<n;i++){
+            pref[i] = pref[i-1]+oneCnt;
+            oneCnt += arr[i];
         }
-
-        return ans;
+        oneCnt = arr[n-1];
+        // suf[n-1]=oneCnt;
+        for(int i=n-2;i>=0;i--){
+            suf[i] = suf[i+1]+oneCnt;
+            oneCnt+=arr[i];
+        }
+        for(int i=0;i<n;i++){
+            arr[i] = pref[i]+suf[i];
+        }
+        return arr;
     }
 }
